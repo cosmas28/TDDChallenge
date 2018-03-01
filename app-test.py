dbe711e4e3968a12d7a9d7ff1e131c93f8e3885a
cosmas28 <augubilla100@gmail.com>
2018-03-01 15:37:43 +0300

    Add test for view contact functionality

--- app-test.py
+++ app-test.py
@@ -11,6 +11,9 @@ class PhonebookTest(unittest.TestCase):
     def test_add_contact(self):
         self.assertEqual(self.phonebook.add_contact(), '0729536851')
 
+    def test_view_contact(self):
+        self.phonebook.add_contact()
+        self.assertEqual([('Cosmas', '0729536851')], self.phonebook.view_contact())
 
 if __name__ == '__main__':
     unittest.main()
